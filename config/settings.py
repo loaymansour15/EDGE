66cc8bd355a6fc02d4dade01ad926aff3264d826
unknown <loaymansour15@yahoo.com>
2026-02-11 00:34:06 +0200

    fixed static files

--- config/settings.py
+++ config/settings.py
@@ -161,7 +161,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+#STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 LOGIN_REDIRECT_URL = "/topics-page/"
 LOGOUT_REDIRECT_URL = "/login/"
